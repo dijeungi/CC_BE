c0cedfcb077b1ceb309d62096b72a0f8fdf1a653
Heeyeon An <gmldus4613@naver.com>
2025-02-17 12:09:11 +0900

    :sparkles: 상연 목록 추가

---
+++ src/main/java/com/example/choiceculture/domain/festival/service/FestivalTimeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.choiceculture.domain.festival.service;
+
+import com.example.choiceculture.domain.festival.dto.FestivalTimeDTO;
+import com.example.choiceculture.domain.festival.entity.FestivalTime;
+import com.example.choiceculture.domain.festival.repository.FestivalTimeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class FestivalTimeServiceImpl implements FestivalTimeService {
+    private final FestivalTimeRepository festivalTimeRepository;
+
+    @Override
+    public List<FestivalTimeDTO> list(Integer festivalId) {
+        List<FestivalTime> timeList=festivalTimeRepository.findByFestivalId(festivalId);
+        return timeList.stream().map(this::entityToDTO).toList();
+    }
+}
